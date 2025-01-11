d90a5d3b9ab9cbfa0f6929fc0f01e3cb6a74f8ac
Felix Müller <felix@fmueller.io>
2025-01-11 14:07:06 +0100

    feat(backend): add markdown-aware chunking

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,12 @@
-import io
-
-import fitz
 import pymupdf4llm
 import requests
 from fastapi import FastAPI
+from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_ollama import ChatOllama
-from langchain_text_splitters import MarkdownTextSplitter
+from langchain_text_splitters import MarkdownHeaderTextSplitter, MarkdownTextSplitter
 
 app = FastAPI()
 
@@ -39,17 +37,37 @@ def rag_test():
                                              encode_kwargs={"normalize_embeddings": True,
                                                             "task": "retrieval.query"})
 
-    question = "Wie sind die Werte der CDU?"
+    question = "Was sind die Werte der CDU?"
 
     response = requests.get(
         "https://www.grundsatzprogramm-cdu.de/sites/www.grundsatzprogramm-cdu.de/files/downloads/240507_cdu_gsp_2024_beschluss_parteitag_final_1.pdf")
-    md_text = pymupdf4llm.to_markdown(fitz.open(stream=io.BytesIO(response.content), filetype="pdf"))
 
-    text_splitter = MarkdownTextSplitter(chunk_size=2000, chunk_overlap=200)
-    splits = text_splitter.split_text(md_text)
-    vector_store = InMemoryVectorStore.from_texts(splits, passage_embeddings)
+    with open("temp.pdf", "wb") as f:
+        f.write(response.content)
 
-    results = vector_store.similarity_search_with_score(query=question, k=10, embeddings=query_embeddings)
+    chunks = pymupdf4llm.to_markdown("temp.pdf", show_progress=False, page_chunks=True)
+    docs = [
+        Document(
+            page_content=chunk["text"],
+            metadata=chunk["metadata"]
+        ) for chunk in chunks
+    ]
+
+    headers_to_split_on = [
+        ("#", "header_1"),
+        ("##", "header_2"),
+        ("###", "header_3"),
+    ]
+
+    header_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on, strip_headers=False)
+    md_chunks = header_splitter.split_text("\n\n".join([doc.page_content for doc in docs]))
+
+    markdown_splitter = MarkdownTextSplitter(chunk_size=2000, chunk_overlap=400)
+    final_chunks = markdown_splitter.split_documents(md_chunks)
+    print(f"Final chunks: {len(final_chunks)}")
+    vector_store = InMemoryVectorStore.from_documents(final_chunks, passage_embeddings)
+
+    results = vector_store.similarity_search_with_score(query=question, k=5, embeddings=query_embeddings)
     context = "\n\n".join(["<chunk>\n" + r[0].page_content + "\n</chunk>" for r in results])
 
     chain = prompt | chat_model
