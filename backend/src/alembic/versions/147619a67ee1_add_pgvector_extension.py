a516a7a0644170de18025dcd8f2a822a17b8853e
Felix Müller <felix@fmueller.io>
2025-01-14 13:54:36 +0100

    feat(backend): add pgvector support
    Also, add alembic for database migrations.

---
+++ backend/src/alembic/versions/147619a67ee1_add_pgvector_extension.py
@@ -0,0 +1,26 @@
+"""add pgvector extension
+
+Revision ID: 147619a67ee1
+Revises: 
+Create Date: 2025-01-14 13:29:29.978101
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '147619a67ee1'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("CREATE EXTENSION IF NOT EXISTS vector")
+
+
+def downgrade() -> None:
+    op.execute("DROP EXTENSION IF EXISTS vector")
--- backend/src/scripts/start-worker.sh
+++ backend/src/scripts/start-worker.sh
@@ -2,4 +2,6 @@
 set -o errexit
 set -o nounset
 
+alembic upgrade head
+
 celery -A tasks.app worker --loglevel=info --concurrency=1
