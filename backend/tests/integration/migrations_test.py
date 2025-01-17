7914543b2e00a9481d70ba00d5d63193a33f5b84
Felix Müller <felix@fmueller.io>
2025-01-17 18:30:36 +0100

    build(backend): add caching to docker build

--- backend/tests/integration/migrations_test.py
+++ backend/tests/integration/migrations_test.py
@@ -6,6 +6,7 @@ from alembic import command
 from alembic.config import Config
 
 
+# TODO add test for ollama docker image
 def test_migration_stairway(database: Generator[Engine], alembic_config: Config) -> None:
     command.downgrade(alembic_config, "base")
     command.upgrade(alembic_config, "head")
