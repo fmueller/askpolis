e21cee6227be2e20ec3c73357b0343cbc8ce3904
Felix Müller <felix@fmueller.io>
2025-01-20 19:42:38 +0100

    fix(backend): minor fix to support windows paths

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]) -> None:
     for item in items:
-        test_path = os.path.abspath(item.location[0])
+        test_path = os.path.abspath(item.location[0]).replace("\\", "/")
 
         if "tests/unit" in test_path:
             item.add_marker(pytest.mark.unit)
