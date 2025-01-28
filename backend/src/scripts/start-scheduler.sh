add937dea208c5b2202d2a5480c908235fc4b9b3
Felix Müller <felix@fmueller.io>
2025-01-28 18:49:13 +0100

    feat(backend): add opentelemetry instrumentalisation to celery scheduler process

--- backend/src/scripts/start-scheduler.sh
+++ backend/src/scripts/start-scheduler.sh
@@ -2,4 +2,15 @@
 set -o errexit
 set -o nounset
 
-celery -A askpolis.celery:app beat --loglevel=info
+export OTEL_PYTHON_LOG_CORRELATION=true
+export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
+
+container_id=$(cat /proc/self/cgroup | grep docker | cut -d'/' -f3 | head -n 1)
+export OTEL_RESOURCE_ATTRIBUTES="container.id=${container_id},${OTEL_RESOURCE_ATTRIBUTES:-}"
+
+opentelemetry-instrument \
+    --traces_exporter otlp_proto_grpc \
+    --metrics_exporter otlp_proto_grpc \
+    --logs_exporter otlp_proto_grpc \
+    celery -A askpolis.celery:app beat \
+    --loglevel=info
