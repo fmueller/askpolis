ef8ba855e7b9461a5ad50d7620ac6302cdc44d7d
Felix Müller <felix@fmueller.io>
2025-01-31 14:19:31 +0100

    chore(backend): rename index to use new table name

---
+++ backend/src/alembic/versions/ff9b556679a6_rename_index.py
@@ -0,0 +1,26 @@
+"""rename_index
+
+Revision ID: ff9b556679a6
+Revises: 0bc41aaee68f
+Create Date: 2025-01-31 14:04:41.178626
+
+"""
+
+from collections.abc import Sequence
+from typing import Union
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "ff9b556679a6"
+down_revision: Union[str, None] = "0bc41aaee68f"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("ALTER INDEX crawling_results_pkey RENAME TO fetched_data_pkey")
+
+
+def downgrade() -> None:
+    op.execute("ALTER INDEX fetched_data_pkey RENAME TO crawling_results_pkey")
