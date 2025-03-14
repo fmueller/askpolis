5eceabb3abf58770f8ef3f40011541807b12c806
Felix Müller <felix@fmueller.io>
2025-03-14 18:46:30 +0100

    chore(backend): add document type to package exports

--- backend/src/askpolis/core/__init__.py
+++ backend/src/askpolis/core/__init__.py
@@ -1,4 +1,14 @@
 from .markdown_splitter import MarkdownSplitter
-from .models import Base, Document, ElectionProgram, Page, Parliament, ParliamentPeriod, Party
+from .models import Base, Document, DocumentType, ElectionProgram, Page, Parliament, ParliamentPeriod, Party
 
-__all__ = ["Base", "Document", "ElectionProgram", "MarkdownSplitter", "Page", "Parliament", "ParliamentPeriod", "Party"]
+__all__ = [
+    "Base",
+    "Document",
+    "DocumentType",
+    "ElectionProgram",
+    "MarkdownSplitter",
+    "Page",
+    "Parliament",
+    "ParliamentPeriod",
+    "Party",
+]
