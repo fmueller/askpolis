c42dfe2500b61c7da5c569e87567ea0b4f782538
Felix Müller <felix@fmueller.io>
2025-03-17 19:27:04 +0100

    feat(backend): add search service

--- backend/tests/integration/search/database_test.py
+++ backend/tests/integration/search/database_test.py
@@ -37,3 +37,31 @@ def test_embeddings_data_model(session_maker: sessionmaker[Session]) -> None:
 
         assert len(embeddings_of_doc) == 1
         np.testing.assert_array_equal(embeddings_of_doc[0].embedding, random_vector)
+
+
+def test_get_all_similar_to(session_maker: sessionmaker[Session]) -> None:
+    with session_maker() as session:
+        collection = EmbeddingsCollection(name="test", version="v1", description="test collection")
+        EmbeddingsCollectionRepository(session).save(collection)
+
+        document = Document(name="Test Document", document_type=DocumentType.ELECTION_PROGRAM)
+        page = Page(document_id=document.id, page_number=1, content="Test Content", page_metadata={"page": 1})
+        session.add(document)
+        session.add(page)
+
+        random_vector = cast(list[float], np.random.rand(1024).astype(np.float32).tolist())
+        embeddings = Embeddings(
+            collection=collection,
+            document=document,
+            page=page,
+            chunk="chunk",
+            embedding=random_vector,
+            chunk_metadata={"key": "value"},
+        )
+        EmbeddingsRepository(session).save_all([embeddings])
+
+    with session_maker() as session:
+        collection = EmbeddingsCollectionRepository(session).get_most_recent_by_name("test")
+        embeddings = EmbeddingsRepository(session).get_all_similar_to(collection, random_vector)
+        assert len(embeddings) == 1
+        np.testing.assert_array_equal(embeddings[0][0].embedding, random_vector)
