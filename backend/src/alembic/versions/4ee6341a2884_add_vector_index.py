fa819b41044cf555d5d7e8cc8b6c1cde6545f479
Felix Müller <felix@fmueller.io>
2025-03-17 19:44:21 +0100

    feat(backend): add database index for cosine operations on embeddings table

---
+++ backend/src/alembic/versions/4ee6341a2884_add_vector_index.py
@@ -0,0 +1,37 @@
+"""add_vector_index
+
+Revision ID: 4ee6341a2884
+Revises: 6dcdadab04b6
+Create Date: 2025-03-17 19:27:58.241753
+
+"""
+
+from collections.abc import Sequence
+from typing import Union
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "4ee6341a2884"
+down_revision: Union[str, None] = "6dcdadab04b6"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with op.get_context().autocommit_block():
+        op.create_index(
+            "hnsw_cosine_idx",
+            "embeddings",
+            [sa.text("embedding vector_cosine_ops")],
+            postgresql_using="hnsw",
+            postgresql_with={"m": 24, "ef_construction": 128},
+            postgresql_concurrently=True,
+        )
+
+
+def downgrade() -> None:
+    with op.get_context().autocommit_block():
+        op.drop_index("hnsw_cosine_idx", table_name="embeddings", postgresql_concurrently=True)
