7094990821eae8ab02bf21c40fe56930c18df71a
Felix Müller <felix@fmueller.io>
2025-03-18 12:58:42 +0100

    refactor(backend): remove legacy search endpoint

--- backend/src/askpolis/main.py
+++ backend/src/askpolis/main.py
@@ -1,12 +1,8 @@
 import os
 
-import requests
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
-from FlagEmbedding import FlagReranker
-from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_ollama import ChatOllama
 from pydantic import BaseModel
@@ -16,7 +12,6 @@ from sqlalchemy.orm import sessionmaker
 from askpolis.celery import app as celery_app
 from askpolis.core import MarkdownSplitter
 from askpolis.core.database import PageRepository
-from askpolis.core.pdf_reader import PdfReader
 from askpolis.logging import configure_logging, get_logger
 from askpolis.search import (
     EmbeddingsCollectionRepository,
@@ -66,18 +61,9 @@ embeddings = HuggingFaceEmbeddings(
     encode_kwargs={"normalize_embeddings": True},
 )
 
-vector_store = InMemoryVectorStore(embeddings)
-
-reranker = FlagReranker("BAAI/bge-reranker-v2-m3")
-
 
 class HealthResponse(BaseModel):
     healthy: bool
-
-
-class LegacySearchResponse(BaseModel):
-    query: str
-    search_results: list[tuple[Document, float]]
 
 
 class SearchResponse(BaseModel):
@@ -88,7 +74,7 @@ class SearchResponse(BaseModel):
 class AnswerResponse(BaseModel):
     question: str
     answer: str
-    search_results: list[tuple[Document, float]]
+    search_results: list[SearchResult]
 
 
 @app.get("/")
@@ -120,60 +106,23 @@ def search(query: str, limit: int = 5, reranking: bool = False) -> SearchRespons
         return SearchResponse(query=query, results=search_service.find_matching_texts(query, limit, reranking))
 
 
-@app.get("/v0/legacy-search")
-def legacy_search(query: str, limit: int = 5, reranking: bool = False) -> LegacySearchResponse:
-    logger.info_with_attrs("Searching...", {"query": query, "limit": limit, "reranking": reranking})
-    if limit < 1:
-        limit = 5
-
-    query_size = limit
-    if reranking:
-        query_size *= 2
-
-    results = vector_store.similarity_search_with_score_by_vector(embedding=embeddings.embed_query(query), k=query_size)
-    if len(results) == 0:
-        logger.warning_with_attrs("No results found", {"query": query})
-
-    if reranking and len(results) > 0:
-        logger.info("Reranking...")
-        reranked_scores = reranker.compute_score([(query, r[0].page_content) for r in results], normalize=True)
-        ranked_docs = [doc for _, doc in sorted(zip(reranked_scores, results), reverse=True)][:limit]
-        return LegacySearchResponse(
-            query=query,
-            search_results=sorted(
-                [(doc[0], score) for doc, score in zip(ranked_docs, reranked_scores)], key=lambda x: x[1], reverse=True
-            ),
-        )
-
-    return LegacySearchResponse(query=query, search_results=results)
-
-
 @app.get("/v0/answers")
 def get_answers(question: str) -> AnswerResponse:
-    logger.info_with_attrs("Querying...", {"question": question})
-    results = vector_store.similarity_search_with_score_by_vector(embedding=embeddings.embed_query(question), k=5)
+    with SessionLocal() as session:
+        default_collection = EmbeddingsCollectionRepository(session).get_most_recent_by_name("default")
+        if default_collection is None:
+            return AnswerResponse(question=question, answer="No default embeddings collection!", search_results=[])
 
-    if len(results) == 0:
-        logger.info("Downloading PDF...")
-        response = requests.get(
-            "https://www.grundsatzprogramm-cdu.de/sites/www.grundsatzprogramm-cdu.de/files/downloads/240507_cdu_gsp_2024_beschluss_parteitag_final_1.pdf"
-        )
+        embeddings_repository = EmbeddingsRepository(session)
+        page_repository = PageRepository(session)
+        embeddings_service = EmbeddingsService(page_repository, embeddings_repository, embeddings, splitter)
+        search_service = SearchService(default_collection, embeddings_service, reranker_service)
+        logger.info_with_attrs("Querying...", {"question": question})
+        results = search_service.find_matching_texts(question, limit=5, use_reranker=True)
 
-        with open("temp.pdf", "wb") as f:
-            f.write(response.content)
+        context = "\n\n".join(["<chunk>\n" + r.matching_text + "\n</chunk>" for r in results])
+        chain = prompt | chat_model
+        logger.info("Invoking LLM chain...")
+        answer = chain.invoke({"documents": context, "question": question})
 
-        logger.info("Read PDF to Markdown...")
-        markdown_doc = PdfReader("temp.pdf").to_markdown()
-        assert markdown_doc is not None
-        final_chunks = MarkdownSplitter(chunk_size=2000, chunk_overlap=400).split(markdown_doc.to_langchain_documents())
-        logger.info(f"Final chunks: {len(final_chunks)}")
-        vector_store.add_documents(final_chunks)
-        logger.info("Querying again...")
-        results = vector_store.similarity_search_with_score_by_vector(embedding=embeddings.embed_query(question), k=5)
-
-    context = "\n\n".join(["<chunk>\n" + r[0].page_content + "\n</chunk>" for r in results])
-    chain = prompt | chat_model
-    logger.info("Invoking LLM chain...")
-    answer = chain.invoke({"documents": context, "question": question})
-
-    return AnswerResponse(question=question, answer=answer.pretty_repr(), search_results=results)
+        return AnswerResponse(question=question, answer=answer.pretty_repr(), search_results=results)
