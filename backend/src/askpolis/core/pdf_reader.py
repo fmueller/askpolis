e3b17f2b71f8c0b05969ee5f66a485e387e7878f
Felix Müller <felix@fmueller.io>
2025-04-25 12:05:15 +0200

    feat(backend): improve hyphenation handling in pdf reader

--- backend/src/askpolis/core/pdf_reader.py
+++ backend/src/askpolis/core/pdf_reader.py
@@ -1,3 +1,4 @@
+import re
 import tempfile
 from typing import Any, Optional, Union
 
@@ -28,16 +29,6 @@ class PdfReader:
     def __init__(self, pdf_source: Union[str, bytes]):
         self.pdf_source = pdf_source
         self.temp_file: Any = None
-
-    def _get_pdf_path(self) -> str:
-        if isinstance(self.pdf_source, bytes):
-            if self.temp_file is None:
-                self.temp_file = tempfile.NamedTemporaryFile(delete=True, suffix=".pdf")  # noqa: SIM115
-                self.temp_file.write(self.pdf_source)
-                self.temp_file.flush()
-            assert self.temp_file is not None
-            return str(self.temp_file.name)
-        return self.pdf_source
 
     def to_markdown(self) -> Optional[PdfDocument]:
         try:
@@ -72,7 +63,21 @@ class PdfReader:
             if self.temp_file is not None:
                 self.temp_file.close()
 
+    def _get_pdf_path(self) -> str:
+        if isinstance(self.pdf_source, bytes):
+            if self.temp_file is None:
+                self.temp_file = tempfile.NamedTemporaryFile(delete=True, suffix=".pdf")  # noqa: SIM115
+                self.temp_file.write(self.pdf_source)
+                self.temp_file.flush()
+            assert self.temp_file is not None
+            return str(self.temp_file.name)
+        return self.pdf_source
+
     def _to_markdown_with_merging_concatenated_words(self) -> Optional[PdfDocument]:
+        def is_hyphenated(w: str) -> bool:
+            hyphen_chars = {"-", "‐", "‑", "‒", "–", "—", "―"}
+            return any(w.endswith(h) for h in hyphen_chars)
+
         # issue: https://github.com/pymupdf/RAG/issues/214
         # from the issue: "TEXT_DEHYPHENATE - this is and will need to be kept off, because otherwise word particles
         # will be included in boundary boxes of lines and spans - making the compilation into markdown impossible.
@@ -80,12 +85,27 @@ class PdfReader:
         parsed_markdown = pymupdf4llm.to_markdown(
             self._get_pdf_path(), show_progress=False, page_chunks=True, extract_words=True
         )
+        page_idx = 0
         for page in parsed_markdown:
+            page_idx += 1
             cleaned_words = []
             words = page["words"]
             i = 0
-            for word in page["text"].split(" "):
+            # we split the text by newline and whitespace characters, but preserve newline characters
+            split_text = re.findall(r"\n|\S+", page["text"])
+            merged_word_suffix = None
+            for word in split_text:
+                if merged_word_suffix is not None:
+                    if merged_word_suffix == word:
+                        merged_word_suffix = None
+                        i += 1
+                    continue
+
                 if i >= len(words):
+                    logger.warning_with_attrs(
+                        "Reached end of words list. This should not happen.",
+                        attrs={"pdf": self.pdf_source, "page": page_idx, "words": len(words), "word_idx": i},
+                    )
                     cleaned_words.append(word)
                     continue
 
@@ -93,23 +113,22 @@ class PdfReader:
                 current_row = int(words[i][5])
                 end_of_row = i < len(words) - 1 and int(words[i + 1][5]) == current_row + 1 and words[i + 1][7] == 0
                 if end_of_row:
-                    if current_word.endswith("-"):
-                        cleaned_words.append(
-                            word.replace("\n", "").replace("-**", "").replace("-~~", "").replace("-__", "")
-                        )
+                    if is_hyphenated(current_word):
+                        # TODO: merge using the split_text directly to not strip of formatting information
+                        merged_word_suffix = words[i + 1][4]
+                        # TODO: also take care of these cases: .replace("-**", "").replace("-~~", "").replace("-__", "")
+                        cleaned_words.append(word.strip("-‐‑‒–—―") + merged_word_suffix)
                         i += 1
+                        continue
                     else:
                         cleaned_words.append(word)
-                        count_newlines = word.count("\n\n")
-                        if (
-                            count_newlines > 0
-                            and i + count_newlines < len(words)
-                            and word.endswith(words[i + count_newlines][4])
-                        ):
-                            i += count_newlines
                 else:
                     cleaned_words.append(word)
-                i += 1
+
+                stripped_formatting = word.strip("*~_`#>-=|[](){}")
+                if stripped_formatting == current_word:
+                    i += 1
+
             page["text"] = " ".join(cleaned_words)
 
         return PdfDocument(
