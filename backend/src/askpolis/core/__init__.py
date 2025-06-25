b8ef78199eaa10a179f551e9089df6f12315f79a
Felix Müller <felix@fmueller.io>
2025-06-25 19:10:41 +0000

    feat(backend): add document and page endpoints

--- backend/src/askpolis/core/__init__.py
+++ backend/src/askpolis/core/__init__.py
@@ -1,8 +1,23 @@
 from askpolis.db import get_db
 
-from .dependencies import get_document_repository, get_parliament_repository
+from .dependencies import (
+    get_document_repository,
+    get_page_repository,
+    get_parliament_repository,
+)
 from .markdown_splitter import MarkdownSplitter
-from .models import Base, Document, DocumentType, ElectionProgram, Page, Parliament, ParliamentPeriod, Party
+from .models import (
+    Base,
+    Document,
+    DocumentResponse,
+    DocumentType,
+    ElectionProgram,
+    Page,
+    PageResponse,
+    Parliament,
+    ParliamentPeriod,
+    Party,
+)
 from .pdf_reader import PdfDocument, PdfPage, PdfReader
 from .repositories import DocumentRepository, PageRepository, ParliamentRepository
 from .routes import router
@@ -10,16 +25,19 @@ from .routes import router
 __all__ = [
     "Base",
     "Document",
+    "DocumentResponse",
     "DocumentRepository",
     "DocumentType",
     "ElectionProgram",
     "get_db",
     "get_document_repository",
+    "get_page_repository",
     "get_parliament_repository",
     "MarkdownSplitter",
     "router",
     "Page",
     "PageRepository",
+    "PageResponse",
     "Parliament",
     "ParliamentRepository",
     "ParliamentPeriod",
