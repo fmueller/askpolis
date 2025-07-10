6810ca0a0b2c5763a65fc143b897a267ef61dbbb
Felix Müller <felix@fmueller.io>
2025-07-10 08:56:11 +0000

    feat(backend): add unique name indexes

--- backend/src/askpolis/core/models.py
+++ backend/src/askpolis/core/models.py
@@ -106,6 +106,7 @@ class Document(Base):
 
 class Parliament(Base):
     __tablename__ = "parliaments"
+    __table_args__ = (Index("uq_parliaments_name", "name", unique=True),)
 
     def __init__(self, name: str, short_name: str, **kw: Any) -> None:
         super().__init__(**kw)
@@ -122,6 +123,7 @@ class Parliament(Base):
 
 class Party(Base):
     __tablename__ = "parties"
+    __table_args__ = (Index("uq_parties_name", "name", unique=True),)
 
     def __init__(self, name: str, short_name: str, **kw: Any) -> None:
         super().__init__(**kw)
